e5a08c71ddc25c520d9d0331143793f9569214f6
Vagner Wentz <wentz.vagner@gmail.com>
2021-05-11 22:55:35 -0300

    feat: add the resources feature to articles.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
-  
-  get "/articles", to: "articles#index"
+
+  resources :articles # Rails provides a routes method
+  # named resources that maps all of the conventional routes for a collection
+  # of a resources.
 end
