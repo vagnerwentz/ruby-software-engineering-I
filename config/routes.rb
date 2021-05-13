0402fb58d7f7726013a23437770354fb46037c8e
Vagner Wentz <wentz.vagner@gmail.com>
2021-05-12 22:55:34 -0300

    feat: the complete crud to article and comments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles # Rails provides a routes method
+  # Rails provides a routes method
   # named resources that maps all of the conventional routes for a collection
   # of a resources.
+  resources :articles do
+    resources :comments
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_001327) do
+ActiveRecord::Schema.define(version: 2021_05_13_014233) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status", default: "public"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
