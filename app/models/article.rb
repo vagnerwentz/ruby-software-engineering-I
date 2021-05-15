a00ea09ce35cd4390ffe3809bdd7587ce587b1dc
Vagner Wentz <wentz.vagner@gmail.com>
2021-05-15 13:16:42 -0300

    feat: add the error message when the field is not followed

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
   include Visible
   has_many :comments, dependent: :destroy
   
-  validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+  validates :title, presence: { :allow_blank => false, :message => "Não pode estar em branco."  }
+  validates :body, presence: { :allow_blank => false, :message => "Não pode estar em branco."  }, length: { minimum: 10, :message => 'A descrição deve conter mais que 10 palavras' }
 end
