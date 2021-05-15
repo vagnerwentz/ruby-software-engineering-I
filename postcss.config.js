38a64fa871279b729e2b3be064d286a15dd85b31
Vagner Wentz <wentz.vagner@gmail.com>
2021-05-15 13:15:31 -0300

    config: add the tailwind css

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
+var tailwindcss = require('tailwindcss');
+
 module.exports = {
   plugins: [
+    tailwindcss('./app/javascript/stylesheets/tailwind.config.js'),
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
